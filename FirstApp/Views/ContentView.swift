ead949f591e3f139ddcfd7005dfbc2129761071e
Khai Nguyen <khainm@digidinos.com>
2022-02-13 23:24:17 -0500

    Change ExerciseView and WelcomeView's content

--- FirstApp/Views/ContentView.swift
+++ FirstApp/Views/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView {
+            WelcomeView()
             ForEach(0 ..< 4) { index in
                 ExerciseView(index: index)
             }
--- FirstApp/Views/WelcomeView.swift
+++ FirstApp/Views/WelcomeView.swift
@@ -5,4 +5,17 @@
 //  Created by Khai Nguyen Minh on 2/13/22.
 //
 
-import Foundation
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text("Hello World!")
+            .padding()
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
